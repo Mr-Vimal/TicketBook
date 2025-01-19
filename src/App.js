24dc4ad3da2b0fd102d912cccb55874f2630b832
Mr-Vimal <mrvimal0501@gmail.com>
2025-01-19 23:16:19 +0530

    new commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className='nav bg-red-400 font-bold w-96 text-blue-700'>
+      Vimal
+    </div>
+    </div>
+  );
+}
+
+export default App;
